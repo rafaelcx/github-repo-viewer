a6605ff4faef97b50813f410dcd74b0a7a3040b4
Rafael Garcia <rafaelgarcia95@gmail.com>
2020-07-17 10:07:36 -0300

    Create apllication Features tests for routing and controllers

---
+++ src/tests/Feature/app/Http/Controllers/PagesControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature\app\Http\Controllers;
+
+use Tests\TestCase;
+
+class PagesControllerTest extends TestCase
+{
+
+    public function testPagesController_Index() {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+        $response->assertViewHasAll([
+            'tittle' => 'Github Repo Finder',
+            'paragraph' => 'This could be a brief explanation about the application',
+        ]);
+    }
+
+    public function testPagesController_Details() {
+        $response = $this->get('/details');
+
+        $response->assertStatus(200);
+        $response->assertViewHas('tittle', 'Main Features');
+        $response->assertViewHas('features', [
+            'repo_finder' => 'Find the most popular Github repositories by language',
+            'login' => 'Login into the platform',
+            'register' => 'Register into the platform under a username and password',
+        ]);
+    }
+
+    public function testPagesController_About() {
+        $response = $this->get('/about');
+
+        $response->assertStatus(200);
+        $response->assertViewHasAll([
+            'tittle' => 'About',
+            'paragraph' => 'This is the about information for the application',
+        ]);
+    }
+
+}
